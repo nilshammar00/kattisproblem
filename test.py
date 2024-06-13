4695efe8d28ade9b455e2a04c9ffdd78babd7b45
nilshammar00 <nilsham@chalmers.se>
2024-06-13 20:43:29 +0200

    ui

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-import sys
-from io import StringIO
+print(2+(int(input())))
+#print(1+(int(input(2))))
 
-# Simulated input data as a multi-line string
-input_data = """3 
-4
-3
-2"""
-
-# Use StringIO to simulate standard input
-sys.stdin = StringIO(input_data)
-print(4/4 + 4 + 4)
